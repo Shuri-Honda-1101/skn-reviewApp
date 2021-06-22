0d5e2246ffd0f6bf413f5245d4962be501491178
Shuri-Honda-1101 <knh065146@gmail.com>
2021-06-23 02:14:54 +0900

    [add]firebaseログイン機能を追加

--- config/firebase.tsx
+++ config/firebase.tsx
@@ -1,15 +1,15 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyCRagsjhBuj3cVPlHkT2FaYsGPX20XGJbk",
+  authDomain: "skn-reviewapp.firebaseapp.com",
+  projectId: "skn-reviewapp",
+  storageBucket: "skn-reviewapp.appspot.com",
+  messagingSenderId: "988445507116",
+  appId: "1:988445507116:web:bd3f586e19195637ba5b89",
+  measurementId: "G-GX1V53SPEX",
 };
 
 firebase.initializeApp(firebaseConfig);
--- src/pages/jumps/Login.tsx
+++ src/pages/jumps/Login.tsx
@@ -1,6 +1,53 @@
+import { useState } from "react";
+import { firebase } from "../../../config/firebase";
 
-const Login = () => {
-return <h1>Loginページ</h1>
-}
+const Login: React.FC<{}> = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
-export default Login
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        alert("ログイン成功");
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("ログイン失敗");
+      });
+  };
+
+  return (
+    <>
+      <h1>Loginページ</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">メールアドレス: </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            placeholder="メールアドレス"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">パスワード: </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="パスワード"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">ログイン</button>
+      </form>
+    </>
+  );
+};
+
+export default Login;
