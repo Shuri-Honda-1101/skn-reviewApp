b6d405d8d0af40697f0885d7883c7deca1276adc
ueuchi <80137361+ueuchi@users.noreply.github.com>
2021-06-23 21:30:05 +0900

    サインアップ機能の作成

---
+++ src/pages/Signup.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { firebase } from "../../config/firebase";
+
+const Signup: React.FC<{}> = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleSubmit = (e: any): void => {
+    e.preventDefault();
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        alert("アカウント作成成功");
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("アカウント作成失敗");
+      });
+  };
+
+  return (
+    <>
+      <h1>Signupページ</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">メールアドレス: </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            placeholder="メールアドレス"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">パスワード: </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="パスワード"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">アカウント作成</button>
+      </form>
+    </>
+  );
+};
+
+export default Signup;
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,12 +1,17 @@
-import Link from "next/Link"
+import Link from "next/Link";
 
 export default function Layout() {
   return (
     <>
-  <h1>skn-review-appへようこそ</h1>
-  <h2>
-    <Link href="/jumps/Login"><a>ログイン</a></Link>
-  </h2>
+      <h1>skn-review-appへようこそ</h1>
+      <h2>
+        <Link href="/jumps/Login">
+          <a>ログイン</a>
+        </Link>
+        <Link href="/Signup">
+          <a>サインアップ</a>
+        </Link>
+      </h2>
     </>
-  )
+  );
 }
